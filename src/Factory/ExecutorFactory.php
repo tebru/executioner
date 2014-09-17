e667e898b0546000c4b0b2deed5b6941fafc80d8
Nate Brunette <n@tebru.net>
2014-09-17 16:15:37 -0500

    Adding Attemptor to factory
    The Executor factory was missing the optional Attemptor argument, which caused
    the attemptor to always be null.
    
    Signed-off-by: Nate Brunette <n@tebru.net>

--- src/Factory/ExecutorFactory.php
+++ src/Factory/ExecutorFactory.php
@@ -5,6 +5,7 @@
 
 namespace Tebru\Executioner\Factory;
 
+use Tebru\Executioner\Attemptor;
 use Tebru\Executioner\Executor;
 use Tebru\Executioner\Logger\ExceptionLogger;
 use Tebru\Executioner\Strategy\TerminationStrategy;
@@ -25,14 +26,16 @@ class ExecutorFactory
      * @param ExceptionLogger $logger
      * @param WaitStrategy $waitStrategy
      * @param TerminationStrategy $terminationStrategy
+     * @param Attemptor $attemptor
      *
      * @return Executor
      */
     public function make(
         ExceptionLogger $logger,
         WaitStrategy $waitStrategy,
-        TerminationStrategy $terminationStrategy
+        TerminationStrategy $terminationStrategy,
+        Attemptor $attemptor = null
     ) {
-        return new Executor($logger, $waitStrategy, $terminationStrategy);
+        return new Executor($logger, $waitStrategy, $terminationStrategy, $attemptor);
     }
 } 
