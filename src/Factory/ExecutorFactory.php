24063d38e1e56ec521dcaa630de8cfe05a41875e
Nate Brunette <n@tebru.net>
2015-01-12 10:30:39 -0600

    Improving factory
    Allowing logger and wait to be created in factory
    
    Signed-off-by: Nate Brunette <n@tebru.net>

--- src/Factory/ExecutorFactory.php
+++ src/Factory/ExecutorFactory.php
@@ -5,8 +5,11 @@
 
 namespace Tebru\Executioner\Factory;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Tebru\Executioner\Exception\InvalidArgumentException;
 use Tebru\Executioner\Executor;
+use Tebru\Executioner\Strategy\WaitStrategy;
 
 /**
  * Class ExecutorFactory
@@ -20,10 +23,49 @@ class ExecutorFactory
     /**
      * Make an Executor
      *
+     * If adding a logger, both name and instance must be passed in.  If adding a wait, only
+     * time or the strategy may be passed in.
+     *
+     * @param null $loggerName
+     * @param LoggerInterface $logger
+     * @param int $waitTime
+     * @param WaitStrategy $waitStrategy
      * @param EventDispatcherInterface $eventDispatcher
      * @return Executor
      */
-    public function make(EventDispatcherInterface $eventDispatcher = null) {
-        return new Executor($eventDispatcher);
+    public function make(
+        $loggerName = null,
+        LoggerInterface $logger = null,
+        $waitTime = null,
+        WaitStrategy $waitStrategy = null,
+        EventDispatcherInterface $eventDispatcher = null
+    ) {
+        $executor = new Executor($eventDispatcher);
+
+        if (!is_null($loggerName) xor !is_null($logger)) {
+            throw new InvalidArgumentException('Logger name and logger must both be set');
+        }
+
+        if (!is_null($waitTime) && !is_null($waitStrategy)) {
+            throw new InvalidArgumentException('Wait time and wait strategy cannot both be set');
+        }
+
+        if (!is_null($loggerName) && !is_null($logger)) {
+            $executor->addLogger($loggerName, $logger);
+        }
+
+        if (is_null($waitTime) && is_null($waitStrategy)) {
+            return $executor;
+        }
+
+        if (null !== $waitTime) {
+            $executor->addWait($waitTime);
+        }
+
+        if (null !== $waitStrategy) {
+            $executor->addWaitStrategy($waitStrategy);
+        }
+
+        return $executor;
     }
 } 
